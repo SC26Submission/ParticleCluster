80e4fb3ff65970cf72a2410962b7f0233803315f
SC26Submission <anonymouscarissa@proton.me>
2026-04-17 20:28:28 -0400

    Delete third_party/LCP/build directory

--- third_party/LCP/build/tools/sz3c/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /global/homes/c/crren/LCP/tools/sz3c
-# Build directory: /global/homes/c/crren/LCP/build/tools/sz3c
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
